66c0b8859c50afcfbf0240e059a15dba2d932031
castillojuan1000 <castillojuan1000@gmail.com>
2021-03-16 16:48:54 -0400

    ISS1 bypass prompt to create dir

--- directory/parent.go
+++ directory/parent.go
@@ -19,21 +19,32 @@ func parentDir(path string) (bool, error) {
 
 	// should return true if file doesn't exist
 	if os.IsNotExist(err) {
-		//prompt user if they'd like dir to be created
-		mk := yesNo()
-
-		if mk == true {
+		//bypass Prompt msg
+		if bypassPrompt() {
 			errDir := os.MkdirAll(path, 0755)
 			//should return nil once directory is made, if not, throw err
 			if errDir != nil {
 				log.Fatal(err)
 			}
-
 		} else {
-			fmt.Println("Please create a directory for the Builder")
-			os.Exit(1)
-			return true, err
+			//prompt user if they'd like dir to be created
+			mk := yesNo()
+
+			if mk == true {
+				errDir := os.MkdirAll(path, 0755)
+				//should return nil once directory is made, if not, throw err
+				if errDir != nil {
+					log.Fatal(err)
+				}
+
+			} else {
+				fmt.Println("Please create a directory for the Builder")
+				os.Exit(1)
+				return true, err
+			}
+
 		}
+
 	}
 
 	//check workspace env exists, if not, create it
@@ -73,4 +84,18 @@ func MakeParentDir() {
 	MakeHiddenDir(path)
 	MakeLogDir(path)
 	MakeWorkspaceDir(path)
+}
+
+func bypassPrompt() bool {
+	args := os.Args[1:]
+
+	yesFlag := false
+
+	for _, val := range args {
+		if val == "--yes" || val == "-y" {
+			yesFlag = true
+		}
+	}
+
+	return yesFlag
 }
