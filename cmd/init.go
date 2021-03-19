24987429de7eef248c691bd4b1a97e38c5fa1895
[Justin Gardner] <justin@gardnerjs.com>
2021-03-19 09:38:18 -0400

    ISS#1-refactored main.go to accept init/config command -added package dependencies

--- cmd/init.go
+++ cmd/init.go
@@ -1,16 +1,23 @@
 package cmd
 
 import (
-	"log"
-	"os"
-	"os/exec"
+	
+	directory "github.com/ilarocca/Builder/directory"
+	utils "github.com/ilarocca/Builder/utils"
 )
 
-func InitCmd() {
-	init := exec.Command("go", "run", "main.go")
-	init.Run()
+func Init() {
+	//check argument syntax, exit if incorrect
+	utils.CheckArgs()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// make dirs
+	directory.MakeParentDir()
+
+	// clone repo into hidden
+	utils.CloneRepo()
+
+	// compile logic to derive project type
+	utils.ProjectType()
+	// copy hidden into work dir, install dependencies, compile source code from repo
+
 }
