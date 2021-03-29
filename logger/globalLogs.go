42ac514362b7008f95390777c4eab5327ff20e54
castillojuan1000 <castillojuan1000@gmail.com>
2021-03-29 14:18:18 -0400

    ISS9 copies individual log into global logs file

--- logger/globalLogs.go
+++ logger/globalLogs.go
@@ -2,8 +2,9 @@ package logger
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
-	"os/exec"
 )
 
 //creates global logs
@@ -11,7 +12,14 @@ func GlobalLogs() {
 	//create global logs if it does not exists
 	if _, err := os.Stat("./globalLogs"); os.IsNotExist(err) {
 		os.Mkdir("./globalLogs", 0755)
+
 	}
+
+	file, err := os.OpenFile("./globalLogs/logs.txt", os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 
 	//create global logs environment
 	val, present := os.LookupEnv("BUILDER_GLOBAL_LOGS_DIR")
@@ -27,5 +35,21 @@ func GlobalLogs() {
 
 	fileName := ConvertParentPathToFileName(parentDir)
 
-	exec.Command("cp", logsDir+"/"+fileName+".txt", "./globalLogs").Run()
+	//append log to global logs
+	from, err := os.Open(logsDir + "/" + fileName + ".txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer from.Close()
+
+	to, err := os.OpenFile("./globalLogs/logs.txt", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer to.Close()
+
+	_, err = io.Copy(to, from)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
