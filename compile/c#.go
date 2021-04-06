dbc7a976b5a96d7817a85132d3d778dc4a4faa04
Ivan La Rocca <ivanlarocca12@gmail.com>
2021-04-06 13:26:12 -0400

    ISS8 reads build tool in yaml

--- compile/c#.go
+++ compile/c#.go
@@ -1,14 +1,32 @@
 package compile
 
 import (
+	"Builder/logger"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 func CSharp(filepath string) {
-	err := exec.Command("dotnet", "build", filepath).Run()
 
+	//install dependencies/build, if yaml build type exists install accordingly
+	buildType := strings.ToLower(os.Getenv("BUILDER_BUILD_TYPE"))
+	var cmd *exec.Cmd
+	if (buildType == "dotnet") {
+		fmt.Println(buildType)
+		cmd = exec.Command("dotnet", "build", filepath)
+	} else {
+		//default
+		cmd = exec.Command("dotnet", "build", filepath)
+	}
+
+	//run cmd, check for err, log cmd
+	logger.InfoLogger.Println(cmd)
+	err := cmd.Run()
 	if err != nil {
+		logger.ErrorLogger.Println("C# project failed to compile.")
 		log.Fatal(err)
 	}
 }
--- utils/setConfigEnvs.go
+++ utils/setConfigEnvs.go
@@ -12,6 +12,17 @@ func ConfigEnvs(byi interface{}) {
 
 	//~~~Check for specific key and create env var based on value~~~
 
+	
+	//check for dir path
+	if val, ok := bldyml["path"]; ok {
+		_, present := os.LookupEnv("BUILDER_DIR_PATH")
+		if !present {
+			//convert val interface{} to string to be set as env var
+			valStr := fmt.Sprintf("%v", val)
+			os.Setenv("BUILDER_DIR_PATH", valStr)
+		}
+	}
+
 	//check for project type
 	if val, ok := bldyml["projectType"]; ok {
 		_, present := os.LookupEnv("BUILDER_PROJECT_TYPE")
@@ -22,13 +33,13 @@ func ConfigEnvs(byi interface{}) {
 		} 
 	}
 
-	//check for dir path
-	if val, ok := bldyml["path"]; ok {
-		_, present := os.LookupEnv("BUILDER_DIR_PATH")
+	//check for build type
+	if val, ok := bldyml["buildType"]; ok {
+		_, present := os.LookupEnv("BUILDER_BUILD_TOOL")
 		if !present {
 			//convert val interface{} to string to be set as env var
 			valStr := fmt.Sprintf("%v", val)
-			os.Setenv("BUILDER_DIR_PATH", valStr)
+			os.Setenv("BUILDER_BUILD_TOOL", valStr)
 		}
 	}
 }
