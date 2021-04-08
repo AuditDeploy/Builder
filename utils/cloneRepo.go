582ae56cdfbfc268bb26972700cbcd489f294c45
castillojuan1000 <castillojuan1000@gmail.com>
2021-04-08 08:48:24 -0400

    ISS14 incomporated -b flag to clone a specific branch

--- utils/cloneRepo.go
+++ utils/cloneRepo.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"Builder/logger"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -16,17 +17,46 @@ func CloneRepo() {
 	hiddenDir := os.Getenv("BUILDER_HIDDEN_DIR")
 
 	//if config cmd clone to temp dir
-	if (hiddenDir == "") {
+	if hiddenDir == "" {
 		errDir := os.Mkdir("./tempRepo", 0755)
 		if errDir != nil {
 			fmt.Println(errDir)
-		}	
+		}
 		cmd := exec.Command("git", "clone", repo, "./tempRepo")
 		cmd.Run()
 	} else {
 		//if init cmd, clone to hidden dir
-		cmd := exec.Command("git", "clone", repo, hiddenDir)
-		logger.InfoLogger.Println(cmd)
-		cmd.Run()
+		bFlagExists, branchName := cloneBranch()
+
+		if bFlagExists {
+			cmd := exec.Command("git", "clone", "-b", branchName, "--single-branch", repo, hiddenDir).Run()
+			logger.InfoLogger.Println(cmd)
+		} else {
+			cmd := exec.Command("git", "clone", repo, hiddenDir).Run()
+			logger.InfoLogger.Println(cmd)
+		}
+
 	}
+}
+
+func cloneBranch() (bool, string) {
+	args := os.Args[1:]
+
+	var branchName string
+	branchFlag := false
+
+	for i, v := range args {
+		if v == "-b" || v == "--branch" {
+			if len(args) <= i+1 {
+				logger.ErrorLogger.Println("No Repo Url Provided")
+				log.Fatal("No Branch Name Provided")
+
+			} else {
+				branchName = args[i+1]
+				branchFlag = true
+			}
+		}
+	}
+
+	return branchFlag, branchName
 }
