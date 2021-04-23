f95032dd53c8312f7fff28863c0f1661c45fa3e3
[Justin Gardner] <justin@gardnerjs.com>
2021-04-23 07:53:06 -0500

    ISS22 Corrected Function spelling

--- utils/createBuilderYaml.go
+++ utils/createBuilderYaml.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func CreateBuilderYmal() {
+func CreateBuilderYaml() {
 	workSpace := os.Getenv("BUILDER_WORKSPACE_DIR")
 
 	file, err := os.OpenFile(workSpace+"/"+"builder.yaml", os.O_RDONLY|os.O_CREATE, 0644)
