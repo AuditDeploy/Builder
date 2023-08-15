403e7c9838ea2bb2707d5b34b7cb7a24dd0f6b25
KatieHarris2397 <katie.harris2397@gmail.com>
2023-08-15 11:49:37 -0400

    Add debug flag that will include caller in locallogger

--- utils/log/log.go
+++ utils/log/log.go
@@ -11,38 +11,51 @@ import (
 var logger *zap.Logger
 
 func NewLogger(logFileName string, path string) *zap.Logger {
-        defaultLogLevel := zapcore.Level(logLevel)
+	defaultLogLevel := zapcore.Level(logLevel)
 
-        config := zap.NewProductionEncoderConfig()
-        config.TimeKey = "timestamp"
-        config.EncodeTime = zapcore.ISO8601TimeEncoder
+	config := zap.NewProductionEncoderConfig()
+	config.TimeKey = "timestamp"
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
 
-        logfile, _ := os.OpenFile(filepath.Join(path, logFileName + ".json"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logfile, _ := os.OpenFile(filepath.Join(path, logFileName+".json"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-        writer := zapcore.AddSync(logfile)
-        fileEncoder := zapcore.NewJSONEncoder(config)
-        core := zapcore.NewTee(
-                zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
-        )
+	writer := zapcore.AddSync(logfile)
+	fileEncoder := zapcore.NewJSONEncoder(config)
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
+	)
 
-        logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	// If debug flag given display caller in log as well
+	args := os.Args[1:]
+	debugFlag := false
+	for i := 0; i < len(args); i++ {
+		if args[i] == "-d" || args[i] == "--debug" {
+			debugFlag = true
+		}
+	}
 
-    return logger
+	if debugFlag {
+		logger = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	} else {
+		logger = zap.New(core, zap.AddStacktrace(zapcore.ErrorLevel))
+	}
+
+	return logger
 }
 
 func init() {
-        args := os.Args[1:]
+	args := os.Args[1:]
 
 	// If verbose flag given display Builder logs to console
 	for i := 0; i < len(args); i++ {
 		if args[i] == "-v" || args[i] == "--verbose" {
-        		logger, _ := zap.NewDevelopment()
+			logger, _ := zap.NewDevelopment()
+			defer logger.Sync()
 
-        		zap.ReplaceGlobals(logger)
+			zap.ReplaceGlobals(logger)
 		}
 	}
 }
-
 
 func Debug(msg string, args ...interface{}) {
 	if logLevel <= DEBUG {
