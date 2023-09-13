edc37f413b420878e23b289ef740dcc44dd825f0
KatieHarris2397 <katie.harris2397@gmail.com>
2023-09-13 14:25:52 -0400

    Update GUI to only display first 8 chars of gitHash in builds list

--- gui/gui.js
+++ gui/gui.js
@@ -98,7 +98,7 @@ function createBuildsListTable(buildsJSON) {
         text += tdString + builds[build].UserName + "</td>"
         text += tdString + builds[build].ArtifactName + "</td>"
         text += tdString + builds[build].ProjectName + "</td>"
-        text += tdString + builds[build].MasterGitHash + "</td>"
+        text += tdString + builds[build].MasterGitHash.slice(0,7) + "</td>"
 
         text += "</tr>"
     }
