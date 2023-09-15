8d521ffa3ed3c24459f5a2e685df6f60b4f8e86b
KatieHarris2397 <katie.harris2397@gmail.com>
2023-09-15 09:06:01 -0400

    Add Build ID to builds list in GUI

--- gui/gui.js
+++ gui/gui.js
@@ -99,6 +99,7 @@ function createBuildsListTable(buildsJSON) {
         text += tdString + builds[build].ArtifactName + "</td>"
         text += tdString + builds[build].ProjectName + "</td>"
         text += tdString + builds[build].MasterGitHash.slice(0,7) + "</td>"
+        text += tdString + builds[build].BuildID + "</td>"
 
         text += "</tr>"
     }
