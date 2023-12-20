eedc64df1e665a1de75987391f4be2d8a117435b
KatieHarris2397 <katie.harris2397@gmail.com>
2023-12-20 16:29:53 -0500

    Update updateParentDir func to not use os.Rename causes errors when used in dockerfile

--- directory/parent.go
+++ directory/parent.go
@@ -2,7 +2,9 @@ package directory
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path"
 	"strings"
 	"time"
 
@@ -109,17 +111,39 @@ func UpdateParentDirName(pathWithWrongParentName string) string {
 		path = wdPath
 	}
 
+	// Rename folder
 	if oldName[0:2] == "./" {
-		err := os.Rename(path+"/"+oldName[2:], path+"/"+newName[2:])
+		// err := os.Rename(path+"/"+oldName[2:], path+"/"+newName[2:])
+		// if err != nil {
+		// 	fmt.Println(err.(*os.LinkError).Err)
+		// 	spinner.LogMessage("could not rename parent dir", "fatal")
+		// }
+
+		err := CopyDir(path+"/"+oldName[2:], path+"/"+newName[2:])
 		if err != nil {
-			fmt.Println(err.(*os.LinkError).Err)
-			spinner.LogMessage("could not rename parent dir", "fatal")
+			spinner.LogMessage("Couldn't copy over files to new parent dir: "+err.Error(), "fatal")
 		}
-	} else {
-		err := os.Rename(oldName, newName)
+
+		err = os.RemoveAll(path + "/" + oldName[2:])
 		if err != nil {
-			fmt.Println(err.(*os.LinkError).Err)
-			spinner.LogMessage("could not rename parent dir", "fatal")
+			spinner.LogMessage("Couldn't delete old parent dir: "+err.Error(), "fatal")
+		}
+
+	} else {
+		// err := os.Rename(oldName, newName)
+		// if err != nil {
+		// 	fmt.Println(err.(*os.LinkError).Err)
+		// 	spinner.LogMessage("could not rename parent dir", "fatal")
+		// }
+
+		err := CopyDir(oldName, newName)
+		if err != nil {
+			spinner.LogMessage("Couldn't copy over files to new parent dir: "+err.Error(), "fatal")
+		}
+
+		err = os.RemoveAll(oldName)
+		if err != nil {
+			spinner.LogMessage("Couldn't delete old parent dir: "+err.Error(), "fatal")
 		}
 	}
 
@@ -134,4 +158,62 @@ func UpdateParentDirName(pathWithWrongParentName string) string {
 	newPath := strings.Replace(pathWithWrongParentName, oldName[2:], newName[2:], 1)
 
 	return newPath
+}
+
+func CopyFile(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
+}
+
+func CopyDir(src string, dst string) error {
+	var err error
+	var fds []os.DirEntry
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = os.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = CopyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = CopyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
 }
