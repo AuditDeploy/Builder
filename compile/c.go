bc7486d371d92325f7eb0292b8534b8ce69c56b1
Katie Harris <katieharris@AuditDeploy.local>
2024-02-16 14:02:30 -0500

    Make fix for C/C++ and C# as well

--- compile/c.go
+++ compile/c.go
@@ -240,7 +240,17 @@ func C(filePath string) {
 	fullPath = directory.UpdateParentDirName(fullPath)
 
 	//creates default builder.yaml if it doesn't exist
-	yaml.CreateBuilderYaml(fullPath)
+	if os.Args[1] == "init" || os.Args[1] == "config" {
+		repoPath := "./" + strings.TrimSuffix(utils.GetName(), ".git")
+
+		if configPath != "" {
+			repoPath = configPath + "/" + strings.TrimSuffix(utils.GetName(), ".git")
+		}
+
+		yaml.CreateBuilderYaml(repoPath)
+	} else {
+		yaml.CreateBuilderYaml(fullPath)
+	}
 
 	packageCArtifact(fullPath)
 
